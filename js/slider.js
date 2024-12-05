a7fd761c9a79d4714f60461b031e2bb524ba95bd
HopeUsersGH <Flash.Matsnev@yandex.ru>
2024-12-05 20:24:16 +0700

    close sample

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+new Swiper('.swiper', {
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
