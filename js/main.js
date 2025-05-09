d03a411981d5a9b4d90198b59d80a0a29e719a4a
HopeUsersGH <flash.matsnev132@gmail.com>
2025-05-09 18:45:11 +0700

    feat: section contact and tabs in projects

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ import ProgressBar from "./ProgressBar.js"
 import TypedAnimateText from "./TypedAnimateText.js"
 import PromoMoveXY from "./PromoMoveXY.js"
 import HorizontalMenuCollection from "./ChangeColorMenu/HorizontalMenu.js"
-import VerticalMenuCollection from "./ChangeColorMenu/VerticalMenu.js";
+import VerticalMenuCollection from "./ChangeColorMenu/VerticalMenu.js"
+import TabsCollection from "./Tabs.js"
 
 new ProgressBar()
 new TypedAnimateText()
 new PromoMoveXY()
 new HorizontalMenuCollection()
 new VerticalMenuCollection()
+new TabsCollection()
